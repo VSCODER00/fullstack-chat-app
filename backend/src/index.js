6915d653ca76c465dd2c05f0fa84f3d3733fb96a
vatsal_shah_1290 <vatsalshah1290@gmail.com>
2024-12-28 02:37:53 +0530

    corrected relative path error

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,7 +28,10 @@ const PORT=process.env.PORT
 if(process.env.NODE_ENV==='production'){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+        res.sendFile(
+          path.join(__dirname, "../", "frontend", "dist", "index.html")
+        );
+
     })
 }
 const start=async ()=>{
